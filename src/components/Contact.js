0ae89640ad8c3909879008c3ec8439861f8b9966
Taja House <ahlliahtaja@yahoo.com>
2020-10-22 02:10:26 -0700

    Added most contend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SocialMedia from '../assets/SocialMedia';
+
 
 const Contact = () =>{
     return(
         <div>
             <a href="mailto: 'ahlliahtaja@yahoo.com'">ahlliahtaja@yahoo.com</a>
             <a href='tel:409-998-4579'>409-998-4579</a>
+
+            <SocialMedia/>
+
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,18 @@ import React, { useState, useEfect, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import moment from 'moment'
 import Clock from 'react-clock'
-
+import './styles/Header.css'
 
 const Header = () =>{
+  //Handles date and time at the top of the page. Will consider adding a digital clock
     const [currentDate, setCurrentDate] = useState('');
     var d = new Date();
     var n = d.getHours();
     const [value, setValue] = useState('');
 
-
     useEffect(() =>{
         const date =  moment().format('YYYY-MM-DD',)
         setCurrentDate(date)
-
 
     }, []);
 
@@ -25,7 +24,6 @@ const Header = () =>{
           () => setValue(new Date()),
           1000
         );
-     
         return () => {
           clearInterval(interval);
         }
@@ -33,15 +31,16 @@ const Header = () =>{
 
     return (
         <div className="header-wrapper">   
-            <div>
+            <div className= "clock-date">
                     <Clock />
                 {currentDate}
             </div>
-            
+            <div className='links'>
                     <Link to="/home">Home</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/about">About Me</Link>
                     <Link to="/contact">Contact Me</Link>  
+            </div>
         </div>
     )
 }
